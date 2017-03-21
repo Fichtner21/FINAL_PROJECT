b3d1d0b5ca432658e129c4776dfe2978f8b807af
Ernest Fichtner <ernest.fichtner@gmail.com>
2017-03-21 11:40:40 +0100

    first commit project

---
+++ storefront/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var sassGlob = require('gulp-sass-glob');
+
+  gulp.task('sass',function(){
+    return gulp.src('assets/sass/**/*.scss')
+    .pipe(sourcemaps.init()) //załadowanie sourcemaps
+    .pipe(sassGlob())
+    .pipe(sass({
+      errLogToConsole:true,
+      outputStyle:'expanded'
+    }))
+    .pipe(sourcemaps.write()) //zapis sourcemaps
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch',['sass'], function(){
+  gulp.watch('assets/sass/**/*.scss',['sass']);
+});
